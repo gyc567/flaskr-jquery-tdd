c5f5e4638c4ceec78258f1c4f74b8ee7f09f133d
eric567 <gyc567@126.com>
2017-04-04 16:37:52 +0800

    add behave lib and example code

---
+++ tests/behave/features/steps/tutorial.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'eric.guo'
+from behave import *
+
+@given('we have behave installed')
+def step_impl(context):
+    pass
+
+@when('we implement a test')
+def step_impl(context):
+    assert True is not False
+
+@then('behave will test it for us!')
+def step_impl(context):
+    assert context.failed is False
